8026944493483dee2dc10f6bc231cd1804ee1721
Declan Mullen <froggear1@gmail.com>
2024-01-11 15:12:05 -0700

    final changes before assignment sharing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function() {
   
     button.addEventListener('click', function() {
       alert("Let's Rock!!");
-      window.open("https://www.youtube.com/", "_blank")
+      //window.open("https://www.youtube.com/", "_blank")
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", "_blank")
     });
   }
